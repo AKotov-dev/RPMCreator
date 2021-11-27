7577b55a0a5ed9fe20db29e41512fb89c4b5878d
AKotov-dev <alex_q_2000@mail.ru>
2021-11-27 10:27:43 +0300

    v1.8-1 pre-release

--- RPMCreator/RPMCreator.pas
+++ RPMCreator/RPMCreator.pas
@@ -18,7 +18,7 @@ var
 {$R *.res}
 
 begin
-  Application.Title:='RPMCreator v1.8';
+  Application.Title:='RPMCreator v1.8-1';
   //Создаём объект с уникальным идентификатором
   MyProg := TUniqueInstance.Create('RPMCreator');
 
